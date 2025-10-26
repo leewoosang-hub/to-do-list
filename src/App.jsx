ca2d8a83de526186d968ed7361d2166e8bd2cbab
leewoosang-hub <dntkds@naver.com>
2025-10-26 22:00:47 +0900

    login components 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
+import LoginPage from './pages/Login'
+import ToDoList from './pages/ToDoList'
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/ToDoList" element={<ToDoList />} />
+      </Routes>
+    </Router>
   )
 }
 
